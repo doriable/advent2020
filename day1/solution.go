d988679af5666151137725d09c2314188d268814
Doria Keung <doria.keung@gmail.com>
2020-12-01 23:20:12 -0800

    Day 1 Part 2

--- day1/solution.go
+++ day1/solution.go
@@ -26,21 +26,31 @@ func main() {
 	}
 
 	for i, a := range input {
-		ret := checkDifference(input, i, a)
+		ret := checkDifference(input, i, a, int64(2020))
 		if ret != int64(-1) {
-			fmt.Printf("the entries are: %v, %v. the product is %v\n", a, ret, a*ret)
-			os.Exit(0)
+			fmt.Printf("the entries for part 1 are: %v, %v. the product is %v\n", a, ret, a*ret)
+			break
 		}
 	}
+
+	for i, a := range input {
+		b, c := checkTriple(input, i, a, int64(2020))
+		if b != -1 {
+			fmt.Printf("the entries for part 2 are: %v, %v, %v. the product is %v\n", a, b, c, a*b*c)
+			break
+		}
+	}
+
+	os.Exit(0)
 }
 
 // checkDifference takes the set of numbers, the number being checking, and the index of
 // this number. It takes the difference of the number from 2020, then scans the rest of the
 // list for the difference from 2020. It skips the duplicate entry based on the index.
 // If there is no other entry that adds up to 2020 with the number, -1 is returned.
-func checkDifference(set []int64, i int, a int64) int64 {
+func checkDifference(set []int64, i int, a, total int64) int64 {
 	ret := int64(-1)
-	diff := 2020 - a
+	diff := total - a
 	for j, b := range set {
 		if i == j {
 			continue
@@ -51,4 +61,18 @@ func checkDifference(set []int64, i int, a int64) int64 {
 		}
 	}
 	return ret
+}
+
+func checkTriple(set []int64, i int, a, total int64) (int64, int64) {
+	for j, b := range set {
+		for k, c := range set {
+			if i == j || i == k {
+				continue
+			}
+			if a+b+c == total {
+				return b, c
+			}
+		}
+	}
+	return -1, -1
 }
